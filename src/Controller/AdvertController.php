ad788fdf2b6cddcbfbe66f250d9464374ba8a589
olga <olgafff@free.fr>
2019-07-30 19:17:16 +0200

    Code refactoring

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Ticket;
 use App\Entity\OrderTicket;
 use App\Form\OrderTicketType;
-use App\Entity\Ticket\Admission;
+use App\Entity\Admission;
+use App\Service\OrderManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,12 +15,14 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
@@ -38,36 +42,38 @@ class AdvertController extends AbstractController
 
         $email_in_session = $this->session->get('email');
 
-
         return $this->render('advert/home.html.twig', [
             'title' => "Hello!"
         ]);
     }
 
+    
+
  /**
      * @Route("/select", name="select")
      */
-    public function select(Request $request){
+    public function select(Request $request, OrderManager $orderManager){
 
       $defaultData = array('message' => 'myform');
 
       $form = $this -> createFormBuilder($defaultData)
                     -> add ('dateEntry', DateType::class) 
-                    -> add ('email')
+                    -> add ('email', EmailType::class)
                     -> add ('fullDay', ChoiceType::class, ['choices' => 
                         ['Journée' => true,
                         'Demi-journée' => false]
                     ])
-                 // -> add ('adultPrice', MoneyType::class)
-                    -> add ('adultAdmission', IntegerType::class, array('attr' => array('class' => 'admissionItem', 'id' => 'admissionItem-adult')))                   
+                    -> add ('adultAdmission', IntegerType::class, array('attr' => array('class' => 'admissionItem', 'id' => 'admissionItem-adult', 'constraints' => 'PositiveOrZero')))                   
                     -> add ('seniorAdmission', IntegerType::class, array('attr' => array('class' => 'admissionItem', 'id' => 'admissionItem-senior')))
                     -> add ('childAdmission', IntegerType::class, array('attr' => array('class' => 'admissionItem', 'id' => 'admissionItem-child')))  
                     -> add ('lowPriceAdmission', IntegerType::class, array('attr' => array('class' => 'admissionItem', 'id' => 'admissionItem-lowPrice')))
                     -> getForm ();                       
         
 
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) 
+      $form->handleRequest($request);
+      if($form->isSubmitted() && $form->isValid()) 
+
+  /*    $order = $orderManager->beginOrder();*/
         {
             $data = $form->getData();
             $this->session->set('dateEntry', $data['dateEntry']);
@@ -77,7 +83,6 @@ class AdvertController extends AbstractController
             $this->session->set('nbSeniorTicket', $data['seniorAdmission']);
             $this->session->set('nbChildTicket', $data['childAdmission']);
             $this->session->set('nbLowPriceTicket', $data['lowPriceAdmission']);
-
 
             return $this->redirectToRoute('step2');
         }
@@ -90,42 +95,93 @@ class AdvertController extends AbstractController
     /**
      * @Route("/step2", name="step2")
      */
-    public function step2(Request $request, ObjectManager $manager){
+    public function step2(Request $request, ObjectManager $manager, Session $session, OrderManager $orderManager){
         
-        $order = new OrderTicket();
-        $order->setEmail($this->session('email'));
+        $em=$this->getDoctrine()->getManager();
 
-        // ticket audlte
-        for($i = 0; $î < $this->session->get('ndAdultTicket'); $î++) {
+        $order = $orderManager->createOrder();
+/*
+        $order = new OrderTicket();
+        
+        $order->setEmail($this->session->get('email'));
+
+
+        // ticket adulte
+        $Adultadmission=$em->getRepository(Admission::class)->findOneBy(['constant_key'=>"ADULT_PRICE"]);
+        for($i = 0; $i < $this->session->get('nbAdultTicket'); $i++) 
+       {
             $ticket = new Ticket(); // cree par n ticket adulte
+            $ticket->setAdmission($Adultadmission);
+            $ticket->setDateEntry($this->session->get('dateEntry'));
             $order->addTicket($ticket);
         }
+        // ticket senior
+        $Senioradmission=$em->getRepository(Admission::class)->findOneBy(['constant_key'=>"SENIOR_PRICE"]);
+        for($j = 0; $j < $this->session->get('nbSeniorTicket'); $j++)
+       {
+            $ticket = new Ticket(); // cree par n ticket adulte
+            $ticket->setAdmission($Senioradmission); 
+            $order->addTicket($ticket);
+        }
+         // ticket child
+        $Childadmission=$em->getRepository(Admission::class)->findOneBy(['constant_key'=>"CHILD_PRICE"]);
+        for($k = 0; $k < $this->session->get('nbChildTicket'); $k++)
+        {
+            $ticket = new Ticket(); // cree par n ticket adulte
+            $ticket->setAdmission($Childadmission);
+            $order->addTicket($ticket);
+        }*/
+
+        $form= $this->createForm(OrderTicketType::class, $order); 
+        $form->handleRequest($request);
+
+
       
+        if($form->isSubmitted() && $form->isValid())   
+        {
 
-
-        dd($order); // dump and die
-
-        $form = $this -> createFormBuilder($order)
-                      -> add ('dateOrder', DateType:: class) 
-                     // -> add ('firstName')
-                      -> add ('price')
-                      -> getForm ();
-
-    //$ticket = new Ticket();
-
-    //$form = $this -> createFormBuilder($ticket)
-    //              -> add ('dateEntry', DateType:: class) 
-    //              -> add ('fullDay')
-    //              -> getForm ();
-    
-    
-    $form->handleRequest($request);
-    //dump($ticket);
-
+            echo 'is valid';
+            $manager->persist($order);
+   ;
+            $manager->flush();
+            return $this->redirectToRoute('step3');
+        } else {
+            echo 'no submit, no valid';
+        }
+        
     return $this->render('advert/step2.html.twig', [
-        'formOrder' => $form->createView()
+        'form' => $form->createView(), 
     ]);
+
 }
+
+    /**
+     * @Route("/step3", name="step3")
+     */
+    public function step3()
+    {
+        return $this->render('advert/step3.html.twig');
+    } 
+
+     /**
+     * @Route("/prepare", name="prepare")
+     */
+    public function prepareAction()
+    {
+        return $this->render('advert/prepare.html.twig');
+    }
+
+    /**
+     * @Route(
+     *     "/checkout",
+     *     name="checkout",
+     *     methods="POST"
+     * )
+     */
+    public function checkoutAction(Request $request)
+    {
+        //TODO : Cette action est à faire
+    }
 
     /**
      * @Route("/advert", name="advert")
@@ -146,47 +202,6 @@ class AdvertController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/select_old", name="select_old")
-     */
-    public function select_old(Request $request, ObjectManager $manager){
-
-        $order = new OrderTicket();
-
-      $form = $this -> createFormBuilder($order)
-                    -> add ('dateOrder', DateType:: class) 
-                    -> add ('email')
-                    -> add ('price')
-                    -> getForm ();
-                       
-    //    $form= $this->createForm(OrderTicketType::class, $order);    
-
-        $form->handleRequest($request);
-        dump($order);
-    //    if($form->isSubmitted() && $form->isValid()) 
-    //    {
-    //        $manager->persist($order);
-    //        $manager->flush();
-
-    //        return $this->redirectToRoute('advert');
-    //    }
-
-        return $this->render('advert/select.html.twig', [
-            'formOrder' => $form->createView()
-        ]);
-    }
-//  public function startOrder(Request $request, Session $session)
-// 	{
-//	    $datas = $form->getData();
-//		$this->session->set('date_entry', $datas['date_entry']);
-//		$ticketsArray = [	
-//								'fullday' => ['ADULT' => 2, 'CHILD' => 3], 
-//								'halfday' => ['ADULT' => 2, 'CHILD' => 3, 'SENIOR' => 1]																																			];
-								
-//		$this->session->set('tickets', $ticketsArray);
-				
-//		$this->redirect('step2');
-//	}
-
+    
 
 }
