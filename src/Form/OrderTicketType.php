ff9b8718cdf88b1717cf388070d84ca3c875533c
olga <olgafff@free.fr>
2019-07-30 19:46:44 +0200

    Added 'tickets', CollectionType

--- src/Form/OrderTicketType.php
+++ src/Form/OrderTicketType.php
@@ -2,22 +2,38 @@
 
 namespace App\Form;
 
+use App\Entity\Ticket;
+use App\Form\TicketType;
 use App\Entity\OrderTicket;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class OrderTicketType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('number')
-            ->add('dateOrder')
-            ->add('email')
+          //  ->add('number')
+         //   ->add('dateOrder')
+            ->add('email', EmailType::class)
             ->add('price')
             ->add('paymentType')
-        ;
+            ->add(
+                'tickets', CollectionType::class, array(
+                            'entry_type' => TicketType::class,
+                            'entry_options' => array('label' =>false),
+                            'label'=>false,
+                            'required'=>false,
+                            'allow_add'=>false,
+                            'allow_delete'=>false,
+                            'prototype'=>true,
+                            'by_reference'=>true,
+                            'mapped'=>true,
+                           )
+                );
     }
 
     public function configureOptions(OptionsResolver $resolver)
